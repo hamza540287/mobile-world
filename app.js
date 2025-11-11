b7005ac29d2fff1fc510591e546c82d9e70917cb
Ali Hamza <ah540223@gmail.com>
2025-11-11 10:49:54 +0500

    Updated something

--- app.js
+++ app.js
@@ -45,6 +45,8 @@ const port= 3000;
         }
     })
   })
+module.exports= app;
+module.exports.handler= serverless(app);
 
 app.listen(port,()=>{
     console.log(`app listening on port ${port}`);
